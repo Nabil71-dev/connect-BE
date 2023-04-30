125fdccddf3aebf2b9b73ac702b8b19bdd654b44
Nabil71-dev <mahmudnabiliiuc@gmail.com>
2023-05-01 00:25:46 +0600

    Initial file structure added

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const cors = require('cors')
+const { unless } = require("express-unless");
+const bodyParser = require('body-parser')
+require('dotenv').config();
+const mongoose = require('mongoose')
+const DbConfig = require('./config/DbConfig');
+const auth = require('./middlewares/auth');
+const { noAuthRoutes } = require("./utils/Constant");
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({ limit: 1024 * 1024 * 10 }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// DATABASE CONNECTION
+
+try {
+  mongoose.connect(DbConfig.db, { useNewUrlParser: true, useUnifiedTopology: true, })
+  console.log("Success");
+} catch (err) {
+  console.log(`Database can't be connected: ${err}`);
+}
+
+// initialize routes
+
+// middleware for error responses
+//default error handler
+app.use((err, req, res, next) => {
+  res.status(500).send({
+    "message": "There was a server side error!"
+  })
+})
+
+app.listen(process.env.SERVER_PORT || 8080, () => {
+  console.log("You are connected");
+});
